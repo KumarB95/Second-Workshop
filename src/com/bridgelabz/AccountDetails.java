05f1d366c2b4dfe670c8a12c3acd844c7c1f009b
KumarB95 <kumarbalingal@gmail.com>
2021-10-08 09:38:28 +0530

    Added : program is to calculate the bank balance

--- src/com/bridgelabz/AccountDetails.java
+++ src/com/bridgelabz/AccountDetails.java
@@ -1,22 +1,25 @@
 package com.bridgelabz;
 
 public class AccountDetails {
-    public int accountBalance;
+    public float accountBalance;
     public float annualInterestRate;
 
-    public AccountDetails(int accountBalance, float annualInterestRate) {
+    public float getAccountBalance() {
+        return accountBalance;
+    }
 
+    public void setAccountBalance(float accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public AccountDetails(float accountBalance, float annualInterestRate) {
         this.accountBalance = accountBalance;
         this.annualInterestRate = annualInterestRate;
     }
 
     public float calculateAccountBalance() {
         float monthlyInterest=(annualInterestRate/12);
-        for (int i=1;i<=12;i++) {
             accountBalance += (accountBalance * monthlyInterest);
-            //System.out.printf("Account balance for %d month : %f",i,accountBalance);
-        }
-        //System.out.println("Total account balance is : "+accountBalance);
         return accountBalance;
     }
 }
--- src/com/bridgelabz/SavingsAccount.java
+++ src/com/bridgelabz/SavingsAccount.java
@@ -6,16 +6,22 @@ public class SavingsAccount {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Banking application");
+        float balanceFirstMonthSaver1,balanceFirstMonthSaver2;
 
 
         AccountDetails accountDetails1=new AccountDetails(2000,4);
-        System.out.println("Account balance for saver 1 Rs for interest rate 4%:"+accountDetails1.calculateAccountBalance());
+        balanceFirstMonthSaver1=accountDetails1.calculateAccountBalance();
+        System.out.println("Account balance for saver 1 Rs for interest rate 4%:"+balanceFirstMonthSaver1);
         AccountDetails accountDetails2=new AccountDetails(3000,4);
-        System.out.println("Account balance for saver 2 Rs for interest rate 4%:"+accountDetails2.calculateAccountBalance());
+        balanceFirstMonthSaver2=accountDetails2.calculateAccountBalance();
+        System.out.println("Account balance for saver 2 Rs for interest rate 4%:"+balanceFirstMonthSaver2);
 
-        AccountDetails accountDetails3=new AccountDetails(2000,5);
-        System.out.println("Account balance for saver 1 Rs for interest rate 5%:"+accountDetails3.calculateAccountBalance());
-        AccountDetails accountDetails4=new AccountDetails(3000,5);
-        System.out.println("Account balance for saver 2 Rs for interest rate 5%:"+accountDetails4.calculateAccountBalance());
+
+        AccountDetails accountDetails3=new AccountDetails(balanceFirstMonthSaver1,5);
+        System.out.println("Account balance next month for saver 1 Rs for interest rate 5%:"+
+                            accountDetails3.calculateAccountBalance());
+        AccountDetails accountDetails4=new AccountDetails(balanceFirstMonthSaver2,5);
+        System.out.println("Account balance next month  Rs for interest rate 5%:"+
+                            accountDetails4.calculateAccountBalance());
     }
 }
